7b7a0871156f26a6d9645235c75835a72849ea83
Aleksey <a.a.korastelev@gmail.com>
2021-08-14 18:54:52 +0300

    add port

--- cmd/urlShortener/main.go
+++ cmd/urlShortener/main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	fmt.Println("start")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!")
 	})
-	err := http.ListenAndServe(":80", nil)
-	if err!= nil{
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
